a187ee43352c10693b1d391437aefd4d66ab80a9
Matias <ma.beas@duocuc.cl>
2026-06-11 12:18:00 -0400

    Se agrego seguridad (probando)

---
+++ src/main/java/cl/Proyecto/api_gateway/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package cl.Proyecto.api_gateway.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Hospital Fricke - API Gateway")
+                        .version("1.0")
+                        .description("Documentación unificada de todos los microservicios"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Auth"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Auth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Ingresa el token obtenido en /auth/login")));
+    }
+}
